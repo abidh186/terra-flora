6823a09243d3a959b70ced3544743105f8e3236c
abidh186 <abidhussain@pursuit.org>
2025-05-07 14:42:07 -0400

    chore: move PingController to controller/ folder

--- server/src/main/java/com/leafandlife/leafandlife_backend/controller/PingController.java
+++ server/src/main/java/com/leafandlife/leafandlife_backend/controller/PingController.java
@@ -1,4 +1,4 @@
-package com.leafandlife.leafandlife_backend;
+package com.leafandlife.leafandlife_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

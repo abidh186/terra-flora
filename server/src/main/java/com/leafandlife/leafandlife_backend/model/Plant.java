f7666512a51bca941e3b50306437f82e84afb4d2
abidh186 <abidhussain@pursuit.org>
2025-05-08 17:11:45 -0400

    chodre: use generated id value in plant model

--- server/src/main/java/com/leafandlife/leafandlife_backend/model/Plant.java
+++ server/src/main/java/com/leafandlife/leafandlife_backend/model/Plant.java
@@ -1,33 +1,30 @@
 package com.leafandlife.leafandlife_backend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Plant {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String type;
 
-    // Constructors
     public Plant() {
     }
 
-    public Plant(Long id, String name, String type) {
-        this.id = id;
+    public Plant(String name, String type) {
         this.name = name;
         this.type = type;
     }
 
-    // Getters and setters
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
